c15dffb508373f3e34c054f6dcbde6e10a264930
Dheeraj Nair <nair.dheeraj@yahoo.co.in>
2017-04-29 19:52:48 +0530

    Updated code to handle incoming requests.

--- power-transmission.php
+++ power-transmission.php
@@ -5,134 +5,92 @@ $servername = "mysql4.gear.host";
 $username = "station1";
 $password = "Rs21_nW6R?13";
 
-if (isset($_GET['check_server'])){
-    $dbname = "substation1";
-    
-    //Create connection
+if(isset($_GET['check_server'])){
+    $dbname = "station1";
     $conn = new mysqli("$servername", $username, $password, $dbname);
+    
+    $sql = "CREATE TABLE SubstationData (ID VARCHAR(10), Users VARCHAR(5), TimeOff DATETIME, TimeOn DATETIME, PowerUsage VARCHAR(4))";
+    $conn->query($sql);
     
     //Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     } 
 
-    echo "Connected successfully";
+    echo "Connected successfully.";
+    $conn->close();
+}
+
+elseif(isset($_GET['delete_values'])){
+    $dbname = "station1";
+    $conn = new mysqli("$servername", $username, $password, $dbname);
+    
+    $sql = "DELETE FROM SubstationData";
+    $conn->query($sql);
+    
+    //Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    echo "Deleted successfully.";
+    $conn->close();
+}
+
+//http://localhost/Practice_Files/power-tranmission/power-transmission.php/?station1&insert_on
+elseif(isset($_GET['station1']) && isset($_GET['admin'])){
+    $dbname = "station1";
+    $conn = new mysqli("$servername", $username, $password, $dbname);
+    
+    $sql = "SELECT * FROM SubstationData";
+
+    $result = mysqli_query($conn ,$sql);
+    $array = [];
+    
+    while($row = $result->fetch_assoc()){
+        $array[] = $row;
+    }
+    
+    header('Content-Type:Application/json');
+    echo json_encode($array);
+    mysqli_free_result($result);
     
     $conn->close();
 }
 
-else{
-    echo "No parameters passed.";
+//http://localhost/Practice_Files/power-tranmission/power-transmission.php/?station1&insert_off&UID="123123"&power_use="123"&num_customer="11"
+elseif(isset($_GET['insert_off']) && isset($_GET['num_customer']) && isset($_GET['UID']) && isset($_GET['power_use']) && isset($_GET['station1'])){
+    $dbname = "station1";
+    $conn = new mysqli("$servername", $username, $password, $dbname);
+    
+    $id = $_GET['UID'];
+    $numbers = $_GET['num_customer'];
+    $power = $_GET['power_use'];
+    
+    $sql = "INSERT INTO SubstationData (ID, Users, TimeOff, TimeOn, PowerUsage) VALUES ($id, $numbers, NOW() - INTERVAL 30 MINUTE, '0000-00-00 00:00:00', $power)";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Data has been added.";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    
+    $conn->close();
 }
 
-//Create table query
-//$sql = "CREATE TABLE BloodBank (Username VARCHAR(40) NOT NULL, Password VARCHAR(40) NOT NULL, Name VARCHAR(40) NOT NULL , BloodType VARCHAR(3) NOT NULL , MobNumber INT(10) NOT NULL, Location VARCHAR(40) NOT NULL, Allergies VARCHAR(40) NULL, PRIMARY KEY (UserName))";
-
-//if ($conn->query($sql) === TRUE) {
-//    echo "Table BloodBank created successfully!";} 
-//else {
-//    echo "Error creating table: " . $conn->error;
-//}
-
-//Getting all data from server
-//Sample URL call is : http://localhost/PHP_Tutorial/web_service.php?admin
-//if(isset($_GET['admin'])){
-//    $sql = "SELECT * FROM BloodBank";
-//
-//    $result = mysqli_query($conn ,$sql);
-//    $array = [];
-//    
-//    while($row = $result->fetch_assoc()){
-//        $array[] = $row;
-//    }
-//    
-//    header('Content-Type:Application/json');
-//    echo json_encode($array);
-//    mysqli_free_result($result);
-//}
-
-//Inserting data
-//Sample URL call is : http://localhost/PHP_Tutorial/web_service.php?type="insert"&username="Mohan1999"&password="123"&name="Mohan Patel"&bloodtype="O%2B"&mobilenumber="1234567890"&location="Vellore"&allergies="None"
-//elseif(isset($_GET['type'])){
-//    $username = $_GET['username'];
-//    $password = $_GET['password'];
-//    $name = $_GET['name'];
-//    $blood = $_GET['bloodtype'];
-//    $mobnumber = $_GET['mobilenumber'];
-//    $location = $_GET['location'];
-//    $allergy = $_GET['allergies'];
-//    
-//    $sql = "INSERT INTO BloodBank (UserName, Password, Name, BloodType, MobNumber, Location, Allergies) VALUES ($username, $password, $name, $blood, $mobnumber, $location, $allergy)";
-//
-//    if ($conn->query($sql) === TRUE) {
-//        echo "You have been registered successfully!";
-//    } else {
-//        echo "Error: " . $sql . "<br>" . $conn->error;
-//    }
-//}
-
-//Validating the user according to the Username
-//Sample URL call is : http://localhost/PHP_Tutorial/web_service.php?username="Dheeraj1998"&password="Dheeraj%401998"
-//elseif(!isset($_GET['login']) && isset($_GET['username']) && isset($_GET['password'])){
-//    $sql = "SELECT Password FROM BloodBank WHERE UserName = " . $_GET['username'];
-//    
-//    $result = mysqli_query($conn ,$sql);
-//    $row = mysqli_fetch_assoc($result);
-//    header('Content-Type:Application/json');
-//    
-//    if($row['Password'] == str_replace("\"", "", $_GET['password'])){
-//        echo 'Success';
-//    }
-//    else{
-//        echo 'Fail';
-//    }
-//    mysqli_free_result($result);
-//}
-
-//Selecting all people with a particular bloodtype
-//Sample URL call is : http://localhost/PHP_Tutorial/web_service.php?bloodtype="B%2B"
-//elseif(isset($_GET['bloodtype'])){
-//    $sql = "SELECT * FROM BloodBank WHERE BloodType = " . $_GET['bloodtype'];
-//
-//    $result = mysqli_query($conn ,$sql);
-//    $array = [];
-//    
-//    while($row = $result->fetch_assoc()){
-//        $array[] = $row;
-//    }
-//    
-//    header('Content-Type:Application/json');
-//    echo json_encode($array);
-//    mysqli_free_result($result);
-//}
-
-//Getting the details of a user by validating his username and password
-//Sample URL call is: http://localhost/PHP_Tutorial/web_service.php?login=''&username='Dheeraj'&password='123'
-//elseif(!isset($_GET['modify']) && isset($_GET['login']) && isset($_GET['username']) && isset($_GET['password'])){
-//    $sql = "SELECT * FROM BloodBank WHERE UserName = " . $_GET['username'] . " AND Password = " . $_GET['password'];
-//
-//    $result = mysqli_query($conn ,$sql);
-//    $row = $result->fetch_assoc();
-//    
-//    header('Content-Type:Application/json');
-//    echo json_encode($row);
-//    mysqli_free_result($result);
-//}
-
-//Modifying the details of a user by validating his username and password
-//Sample URL call is: http://dheerajprojects.gear.host/web_server.php?modify=''&username='Dheeraj1998'&mpass='123'&maller='Cold'&mblood='A%2B'&mmobilenumber="1231231231"&mname="Dheeraj"&mloc="Jamnagar"
-//elseif(isset($_GET['modify']) && isset($_GET['username'])){
-//    $sql = "UPDATE BloodBank SET Password = " . $_GET['mpass'] . ", Name = " . $_GET['mname'] . ", Location = " . $_GET['mloc'] . ", Allergies = " . $_GET['maller'] . ", MobNumber = " . $_GET['mmobilenumber'] . ", BloodType = " . $_GET['mblood'] . " WHERE UserName = " . $_GET['username'];
-//    
-//    if ($conn->query($sql) === TRUE) {
-//        echo "Details have been modified!";
-//    } else {
-//        echo "Error: " . $sql . "<br>" . $conn->error;
-//    }
-//}
-//
-//else{
-//    echo "Hi! No parameters have been passed :)";
-//}
-
+//http://localhost/Practice_Files/power-tranmission/power-transmission.php/?station1&insert_on
+elseif(isset($_GET['insert_on']) && isset($_GET['station1'])){
+    $dbname = "station1";
+    $conn = new mysqli("$servername", $username, $password, $dbname);
+    
+    $sql = "UPDATE SubstationData SET TimeOn = NOW() - INTERVAL 30 MINUTE WHERE TimeOn = '0000-00-00 00:00:00'";
+    
+    if ($conn->query($sql) === TRUE) {
+        echo "Switch on data has been added.";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    
+    $conn->close();
+}
 ?>
